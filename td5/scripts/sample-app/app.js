3c124c1e19583096a07c56c4631a4e6d3303c81c
vlad0sn <vladsenechal@gmail.com>
2025-01-20 15:19:49 +0100

    Introduce intentional error

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end('Devops Labs!\n');
 });
 
 const port = process.env.PORT || 8080;
